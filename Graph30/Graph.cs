578c6040ee74fd82ad36f94c39b58b59b95c35bb
GolikovaVV <GolikovaVV@402D-9>
2023-03-30 12:04:24 +0700

    Добавьте файлы проекта.

---
+++ Graph30/Graph.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph30
+{
+    class Graph<T>
+    {
+        private Dictionary<T, List<T>> adj; // словарь смежности
+
+        public Graph()
+        {
+            adj = new Dictionary<T, List<T>>();
+        }
+
+        public void AddVertex(T v)
+        {
+            adj.Add(v, new List<T>());
+        }
+
+        public void AddEdge(T v, T w)
+        {
+            adj[v].Add(w);
+        }
+
+        public void DFS(T v, HashSet<T> visited)
+        {
+            visited.Add(v);
+            Console.Write(v + " ");
+
+            foreach (T i in adj[v])
+            {
+                if (!visited.Contains(i))
+                    DFS(i, visited);
+            }
+        }
+
+        public void Traverse()
+        {
+            HashSet<T> visited = new HashSet<T>();
+            foreach (T v in adj.Keys)
+            {
+                if (!visited.Contains(v))
+                    DFS(v, visited);
+            }
+        }
+    }
+}
---
+++ Graph30/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Graph30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество вершины (введите готово, чтобы остановиться): ");
+            Graph<string> graph = new Graph<string>();
+
+            while (true)
+            {
+                string v = Console.ReadLine();
+                if (v == "Выполнено")
+                    break;
+                graph.AddVertex(v);
+            }
+
+            Console.WriteLine("Введите края (введите готово, чтобы остановиться): ");
+            while (true)
+            {
+                string v = Console.ReadLine();
+                if (v == "Выполнено")
+                    break;
+                string w = Console.ReadLine();
+                graph.AddEdge(v, w);
+            }
+
+            Console.WriteLine("Traversal: ");
+            graph.Traverse();
+        }
+    }
+}
+/*
+ напишу простую и короткую программу на си шарп для полного обхода графа с пользовательским вводом в консоль. 
+Выведи класс Graph и точку входа. Оптимизируй программу так, чтобы не было повторного поиска через те же пути графа.
+ */
